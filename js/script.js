081edcbef82e51f9009ebd11261aa77783fad518
Parvathy Santhosh <parsan1998@gmail.com>
2018-09-09 18:10:22 +0100

    Redundancy in html removed - cat array

--- js/script.js
+++ js/script.js
@@ -1,12 +1,42 @@
-var count_one = 0;
-var count_two = 0;
+let Cats = [
+  {
+    "name" : "Crookshanks",
+    "image" : "https://vignette.wikia.nocookie.net/harrypotter/images/7/71/CrookshanksPottermore.png",
+    "counter" : "0"
+  },
+  {
+    "name" : "Garfield",
+    "image" : "https://png.pngtree.com/element_origin_min_pic/16/11/24/ea803516a485cd425ce348ea32075b87.jpg",
+    "counter" : "0"
+  },
+  {
+    "name" : "Sylvester",
+    "image" : "https://img00.deviantart.net/7664/i/2002/40/9/5/silly_sylvester.jpg",
+    "counter" : "0"
+  },
+  {
+    "name" : "Tom",
+    "image" : "https://png2.kisspng.com/20180406/row/kisspng-tom-cat-tom-and-jerry-nibbles-cartoon-tom-and-jerry-5ac786490a90e8.2434223615230254810433.png",
+    "counter" : "0"
+  },
+  {
+    "name" : "Hello Kitty",
+    "image" : "http://media.comicbook.com/uploads1/2015/02/hello-kitty-124499.jpg",
+    "counter" : "0"
+  }
+];
 
-$('#cat-one').click(function(e) {
-  count_one += 1;
-  $('#one-display').val(count_one);
-});
+var elem = document.createElement('div');
+document.innerHTML = "";
 
-$('#cat-two').click(function(e) {
-  count_two += 1;
-  $('#two-display').val(count_two);
-});
+for (i = 0; i < Cats.length; i++) {
+  elem.innerHTML += "<h3>" + Cats[i]['name'] + "</h3>";
+  elem.innerHTML += "<img onClick=\"addCount(" + i + ")\" class=\"cat-photo\" src=\"" + Cats[i]['image'] + "\"><br>";
+  elem.innerHTML += "<textarea id=\"text" + i + "\">" + Cats[i]['counter'] + "</textarea><hr>"
+  document.getElementById('cats').appendChild(elem);
+};
+
+function addCount(i) {
+  Cats[i]['counter']++;
+  $('#text'+i).text(Cats[i]['counter']);
+}
